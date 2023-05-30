250f43e1e72cbb7aa5a7b265cc11ba3230c3c395
Muhammetmyrat Shamuradov <115918117+cl0777@users.noreply.github.com>
2023-05-30 16:44:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,69 +1,83 @@
-const express = require('express')
-const { App, ExpressReceiver } = require('@slack/bolt');
+const { RTMClient } = require("@slack/rtm-api");
+const { WebClient } = require("@slack/web-api");
+const { Configuration, OpenAIApi } = require("openai");
 
-const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
 
-receiver.router.use(express.static('public'))
+// Initialize Slack API client
+const slackClientRTM = new RTMClient(
+  "xoxb-5159745880645-5207057321461-2YmVFPIF3KMgT8nzzJYSNabK"
+);
+const slackClientWEB = new WebClient(
+  "xoxb-5159745880645-5207057321461-2YmVFPIF3KMgT8nzzJYSNabK"
+);
 
-const app = new App({
-  receiver,
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+// Function to send a message using the Slack API
+async function sendMessage(channel, text) {
+  try {
+    await slackClientWEB.chat.postMessage({
+      channel: channel,
+      text: text,
+    });
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
+}
+
+// Function to send a message to the OpenAI ChatGPT API
+async function sendChatMessage(message) {
+  try {
+    const config = new Configuration({
+      apiKey: "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",
+    });
+
+    const openai = new OpenAIApi(config);
+
+    const runPrompt = async (message) => {
+      const prompt = message;
+
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 4000,
+        temperature: 1,
+      });
+
+      const parsableJSONresponse = response.data.choices[0].text;
+
+      return parsableJSONresponse;
+    };
+
+    return runPrompt(message);
+  } catch (error) {
+    console.error("Error sending chat message:", error);
+  }
+}
+
+// Listen for message events
+slackClientRTM.on("message", async (event) => {
+  // Ignore bot messages and messages without text
+  if (!event.bot_id && event.text) {
+    // Get the prompt from the Slack message using ChatGPT API
+    const prompt = event.text;
+
+    // Send the prompt to ChatGPT API
+    const response = await sendChatMessage(prompt);
+
+    // Send the response back to the Slack channel
+    sendMessage(event.channel, response);
+  }
 });
 
+// Start the bot
+async function startBot() {
+  try {
+    // Connect to Slack
+    await slackClientRTM.start();
 
-app.message(':wave:', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say({
-    blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
-          },
-          "action_id": "button_click"
-        }
-      }
-    ],
-    text: `Hey there <@${message.user}>!`,
-    thread_ts: message.ts
-  });
-});
+    console.log("Bot is running!");
+  } catch (error) {
+    console.error("Error starting bot:", error);
+  }
+}
 
-app.event('app_mention', async ({ event, say }) => {
-  await say({
-    text: `Hello <@${event.user}>: If you send me a :wave: I'll send you a button to click. If you add a reaction to a message I'll say thanks.`,
-    thread_ts: event.ts
-  })
-});
-
-app.event('reaction_added', async ({ event, say }) => {
-  await say({
-    text: `Thanks for the :${event.reaction}:`,
-    thread_ts: event.item.ts
-  })
-});
-
-app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  // console.log(JSON.stringify(body,null,2))
-  await say({
-    text: `<@${body.user.id}> you clicked the button. Well done.`,
-    thread_ts: body.message.ts
-  });
-});
-
-(async () => {
-  // Start the app
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
+startBot();
